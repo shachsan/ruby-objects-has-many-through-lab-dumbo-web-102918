191f9663a5bfde363fc0d8d6a756eb143cbb8428
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 02:37:03 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,15 @@
+class Doctor
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  def self.all
+    @@all
+  end
+end
