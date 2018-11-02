ca9dec12650f1cb259a3e93d00b1516e38b5a9ac
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 02:50:18 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,8 +12,9 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select{|appointment|appointment.patient}
+    Appointment.all.select{|appointment|appointment.patient==self}
   end
+  
 
   def self.all
     @@all
